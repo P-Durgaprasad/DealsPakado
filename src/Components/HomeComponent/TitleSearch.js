112168b2ff5bc3324429843b17f293f1541e2c60
DurgaPrasad Penumaka <durgaprasadpenumaka92@gmail.com>
2023-12-21 20:54:25 +0530

    routing

--- src/Components/HomeComponent/TitleSearch.js
+++ src/Components/HomeComponent/TitleSearch.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import DealsDisplay from './DealsDisplay';
+import Home from './Home';
+import API from '../API_Config';
 
 const TitleSearch = () => {
-  const { affurl } = useParams();
+  const { title } = useParams();
+  const [productDetails, setProductDetails] = useState(null);
+  const [error, setError] = useState(null);
 
-  try {
-    window.open(decodeURIComponent(affurl), '_blank');
-  } catch (err) {
-    console.log('An unexpected error occurred.');
-  }
+  useEffect(() => {
+    const fetchProductDetails = async () => {
+      try {
+        if (title) {
+          const url = `${API}/api/product/title?title=${encodeURIComponent(title)}`;
+          const response = await fetch(url);
+          const data = await response.json();
+
+          // Assuming you want the first item in the array
+          if (Array.isArray(data) && data.length > 0) {
+            setProductDetails(data[0]);
+          } else {
+            setError('Invalid or missing product details in the response.');
+          }
+        }
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchProductDetails();
+  }, [title]);
+
+  useEffect(() => {
+    if (productDetails && productDetails.affiliateLink) {
+      console.log(productDetails.affiliateLink);
+      window.open(productDetails.affiliateLink, '_blank');
+    } else if (error) {
+      console.error(error);
+    }
+  }, [productDetails, error]);
 
   return (
     <div>
-      <DealsDisplay />
+        <Home />
     </div>
   );
 };
