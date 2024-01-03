8923dec60e00d616152acaf8ba612307e6d55fd5
DurgaPrasad Penumaka <durgaprasadpenumaka92@gmail.com>
2024-01-03 11:29:47 +0530

    solve redirect issue

--- src/Components/HomeComponent/TitleSearch.js
+++ src/Components/HomeComponent/TitleSearch.js
@@ -6,46 +6,47 @@ import API from '../API_Config';
 const TitleSearch = () => {
   const { title } = useParams();
   const [productDetails, setProductDetails] = useState(null);
-  const [error, setError] = useState(null);
+  const [hasRedirected, setHasRedirected] = useState(false);
 
   useEffect(() => {
     const fetchProductDetails = async () => {
       try {
         if (title) {
-          console.log(title);
           const url = `${API}/api/product/title?title=${encodeURIComponent(title)}`;
-          console.log(title);
           const response = await fetch(url);
           const data = await response.json();
 
           if (Array.isArray(data) && data.length > 0) {
             setProductDetails(data[0]);
           } else {
-            setError('Invalid or missing product details in the response.');
+            console.error('Invalid or missing product details in the response.');
           }
         }
       } catch (error) {
-        setError(error.message);
+        console.error(error.message);
       }
     };
 
-    fetchProductDetails();
-  }, [title]);
+    const redirectToAffiliate = async () => {
+      if (productDetails && productDetails.affiliateLink) {
+        window.open(productDetails.affiliateLink, '_blank');
+        setHasRedirected(true); 
+      } else {
+        console.error('No affiliate link available for redirection.');
+      }
+    };
 
-  useEffect(() => {
-    if (productDetails && productDetails.affiliateLink) {
-      console.log(productDetails.affiliateLink);
-      window.open(productDetails.affiliateLink, '_blank');
-    } else if (error) {
-      console.error(error);
+    if (!hasRedirected && productDetails) {
+      redirectToAffiliate();
+    } else {
+      fetchProductDetails();
     }
-  }, [productDetails, error]);
+  }, [title, productDetails, hasRedirected]);
 
   return (
     <div>
-        <Home />
+      <Home />
     </div>
   );
 };
-
 export default TitleSearch;
